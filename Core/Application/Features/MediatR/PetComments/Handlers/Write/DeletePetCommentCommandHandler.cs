cf4eec8f3613e0ed8b6ee8623a6b867faf1f118d
kosebaris1 <kosebaris464@gmail.com>
2025-05-06 22:21:28 +0300

    Update-Delete PetComment EndPoint Completed

--- Core/Application/Features/MediatR/PetComments/Handlers/Write/DeletePetCommentCommandHandler.cs
+++ Core/Application/Features/MediatR/PetComments/Handlers/Write/DeletePetCommentCommandHandler.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Application.Features.MediatR.PetComments.Commands;
+using Application.Features.MediatR.Pets.Commands;
+using Application.Interfaces;
+using Domain;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,21 @@ using System.Threading.Tasks;
 
 namespace Application.Features.MediatR.PetComments.Handlers.Write
 {
-    public class DeletePetCommentCommandHandler
+    public class DeletePetCommentCommandHandler : IRequestHandler<DeletePetCommentCommand,Unit>
     {
+        private readonly IRepository<PetComment> _repository;
+
+        public DeletePetCommentCommandHandler(IRepository<PetComment> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Unit> Handle(DeletePetCommentCommand request, CancellationToken cancellationToken)
+        {
+            var petComment = await _repository.GetByIdAsync(request.PetCommentId);
+            await _repository.DeleteAsync(petComment);
+            return Unit.Value;
+        }
+
     }
 }
--- Presentation/WebApi/Controllers/PetCommentsController.cs
+++ Presentation/WebApi/Controllers/PetCommentsController.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Constants;
 using Application.Features.MediatR.PetComments.Commands;
+using Application.Features.MediatR.Pets.Commands;
 using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Http;
@@ -23,6 +24,21 @@ namespace WebApi.Controllers
         {
             await _mediator.Send(command);
             return Ok(Messages<PetComment>.EntityAdded); 
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdatePetComment(UpdatePetCommentCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok(Messages<Pet>.EntityUpdated);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeletePetCommmnet(int petCommentId)
+        {
+            await _mediator.Send(new DeletePetCommand(petCommentId));
+            return Ok(Messages<Pet>.EntityDeleted);
+
         }
     }
 
