7e3ee485c0a76328855d0bb84382cdff8edf4e45
Mert Sayan <Mertrutenyumsayan@gmail.com>
2025-05-25 13:38:20 +0300

    Register view  entegreted

---
+++ FrontEnd/HayvanDto/UserDtos/CreateUserDto.cs
@@ -0,0 +1,14 @@
+﻿namespace HayvanDto.UserDtos
+{
+    public class CreateUserDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string? ProfilePictureUrl { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+        public string? AboutMe { get; set; } // Profil açıklaması
+    }
+}
--- FrontEnd/HayvanWebUI/Controllers/RegisterController.cs
+++ FrontEnd/HayvanWebUI/Controllers/RegisterController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HayvanDto.UserDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace HayvanWebUI.Controllers
 {
     public class RegisterController : Controller
     {
-        //doldurulacak
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public RegisterController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateUserDto dto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(dto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7160/api/Auths/CreateUser", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Home");
+            }
             return View();
         }
     }
