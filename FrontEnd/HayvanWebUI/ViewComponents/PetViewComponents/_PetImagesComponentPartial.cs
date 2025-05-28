44e1a6fdfcd41fc10fce5c2ca2996e8fdcb3a558
Mert Sayan <Mertrutenyumsayan@gmail.com>
2025-05-28 15:24:38 +0300

    otherPetImages section entegreted petDetailView

--- FrontEnd/HayvanWebUI/ViewComponents/PetViewComponents/_PetImagesComponentPartial.cs
+++ FrontEnd/HayvanWebUI/ViewComponents/PetViewComponents/_PetImagesComponentPartial.cs
@@ -1,19 +1,29 @@
-﻿//using Microsoft.AspNetCore.Mvc;
+﻿using HayvanDto.PetImageDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
-//namespace HayvanWebUI.ViewComponents.PetViewComponents
-//{
-//    public class _PetImagesComponentPartial:ViewComponent
-//    {
-//        private readonly IHttpClientFactory _httpClientFactory;
+namespace HayvanWebUI.ViewComponents.PetViewComponents
+{
+    public class _PetImagesComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
 
-//        public _PetImagesComponentPartial(IHttpClientFactory httpClientFactory)
-//        {
-//            _httpClientFactory = httpClientFactory;
-//        }
+        public _PetImagesComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
 
-//        public async Task<IViewComponentResult> InvokeAsync(int id)
-//        {
-
-//        }
-//    }
-//}
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7160/api/PetImages?id=" + id);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<List<GetAllPetImageByPetIdDto>>(jsonData);
+                return View(result);
+            }
+            return View();
+        }
+    }
+}
