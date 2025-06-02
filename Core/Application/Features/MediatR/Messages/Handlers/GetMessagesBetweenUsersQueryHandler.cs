0f68a7268578f68d36ad1cd1f7a4ac842e3e3069
Mert Sayan <Mertrutenyumsayan@gmail.com>
2025-06-02 15:34:13 +0300

    Messages were fetched from the database for instant messaging

---
+++ Core/Application/Features/MediatR/Messages/Handlers/GetMessagesBetweenUsersQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using Application.Features.MediatR.Messages.Queries;
+using Application.Interfaces.MessageInterface;
+using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.MediatR.Messages.Handlers
+{
+    public class GetMessagesBetweenUsersQueryHandler:IRequestHandler<GetMessagesBetweenUsersQuery,List<MessageDto>>
+    {
+        private readonly IMessageRepository _messageRepository;
+        private readonly IMapper _mapper;
+
+        public GetMessagesBetweenUsersQueryHandler(IMessageRepository messageRepository, IMapper mapper)
+        {
+            _messageRepository = messageRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<MessageDto>> Handle(GetMessagesBetweenUsersQuery request, CancellationToken cancellationToken)
+        {
+            var messages = await _messageRepository.GetConversationAsync(request.UserId1, request.UserId2);
+            return messages.Select(m => new MessageDto
+            {
+                SenderId = m.SenderId,
+                MessageText = m.MessageText,
+                CreatedDate = m.CreatedDate ?? DateTime.MinValue
+            }).ToList();
+        }
+    }
+}
---
+++ Presentation/WebApi/Controllers/MessagesController.cs
@@ -0,0 +1,31 @@
+﻿using Application.Features.MediatR.Messages.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public MessagesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("GetConversation")]
+        public async Task<IActionResult> GetConversation(int userId1, int userId2)
+        {
+            var result = await _mediator.Send(new GetMessagesBetweenUsersQuery
+            {
+                UserId1 = userId1,
+                UserId2 = userId2
+            });
+
+            return Ok(result);
+        }
+    }
+}
